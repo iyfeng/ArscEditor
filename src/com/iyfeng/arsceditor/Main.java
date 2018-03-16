38b207370bb02413c621c6b97b8c346d2f877a4c
iyfeng <feng@fanyunfengdeMacBook-Pro.local>
2018-03-17 03:09:57 +0800

    暂时搞定

--- src/com/iyfeng/arsceditor/Main.java
+++ src/com/iyfeng/arsceditor/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Utils utils = new Utils();
+		utils.openArsc("/Users/feng/Documents/github/ArscEditor/resources1.arsc");
+		utils.getResouces("string", "[DEFAULT]");
+		utils.changeResouce("app_name", "很完但快跑");
+		utils.saveArsc("/Users/feng/Documents/github/ArscEditor/resources1.arsc",
+				"/Users/feng/Documents/github/ArscEditor/resources1.arsc");
 	}
 
 }
